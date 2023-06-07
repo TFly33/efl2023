4e3f094034858bb51ec3e67b351a55184438f5ce
Scott <smb209@gmail.com>
2023-06-06 21:50:22 -0700

    Tmp

---
+++ client/build/precache-manifest.0f4886f192783b54f5470780b56cfa39.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "bc17d802004ff457fcf3",
+    "url": "/static/css/main.ef930522.chunk.css"
+  },
+  {
+    "revision": "bc17d802004ff457fcf3",
+    "url": "/static/js/main.3dc7fe7b.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "4c120011e318b0675c94",
+    "url": "/static/css/2.6cea05fa.chunk.css"
+  },
+  {
+    "revision": "4c120011e318b0675c94",
+    "url": "/static/js/2.03604f46.chunk.js"
+  },
+  {
+    "revision": "f3f0dcd7f805aab65bc04351c106758e",
+    "url": "/index.html"
+  }
+];
